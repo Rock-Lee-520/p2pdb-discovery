53da9dc1939dcc8c5ffe2435472c170279e12522
pandaLIU <563883861@qq.com>
2022-09-11 16:47:51 +0800

    Added dht

--- discovery.go
+++ discovery.go
@@ -2,31 +2,24 @@ package discovery
 
 import (
 	"context"
-	"log"
-	"time"
-
+	"crypto/rand"
+	"github.com/Rock-liyi/p2pdb/infrastructure/util/log"
 	"github.com/libp2p/go-libp2p"
+	libp2pConnmgr "github.com/libp2p/go-libp2p-connmgr"
+	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
-	"github.com/libp2p/go-libp2p/p2p/discovery/mdns"
+	"github.com/libp2p/go-libp2p-core/routing"
+	discovery "github.com/libp2p/go-libp2p-discovery"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	"github.com/libp2p/go-libp2p/p2p/muxer/yamux"
+	libp2pTls "github.com/libp2p/go-libp2p/p2p/security/tls"
+	libp2pTcp "github.com/libp2p/go-libp2p/p2p/transport/tcp"
+	"github.com/multiformats/go-multiaddr"
+	"github.com/sirupsen/logrus"
+	"sync"
+	"sync/atomic"
 )
-
-// DiscoveryInterval is how often we re-publish our mDNS records.
-const DiscoveryInterval = time.Hour
-
-// DiscoveryServiceTag is used in our mDNS advertisements to discover other chat peers.
-const DiscoveryServiceTag = "p2pdb-example"
-
-type Discovery interface {
-	// create a new libp2p Host that listens on a random TCP port
-	//	Connect() (Host string)
-	Create(host string) (host.Host, error)
-	SetupDiscovery(host host.Host) error
-}
-
-type DiscoveryFactory struct {
-	h host.Host
-}
 
 const LISTEN_ADDRESS_STRINGS = "/ip4/0.0.0.0/tcp/0"
 
@@ -35,59 +28,137 @@ type discoveryNotifee struct {
 	h host.Host
 }
 
-func (d *DiscoveryFactory) GetLocalPeerId() peer.ID {
-	return d.h.ID()
-}
-
-func (d *DiscoveryFactory) SetLocalPeerId(id peer.ID) {
-
-}
-
-func NewDiscoveryFactory() *DiscoveryFactory {
-	return &DiscoveryFactory{}
-}
-
-func CreatePeerKey(key string) (string, error) {
-	return key, nil
-}
-
-func GetPublicKey(key string) (string, error) {
-	return key, nil
-}
-
-func GetPrivateKey(key string) (string, error) {
-	return key, nil
-}
-
-// func (d *DiscoveryFactory) Start(publicKey string) (host.Host, error) {
-
-// 	h, err := libp2p.New(libp2p.ListenAddrStrings(LISTEN_ADDRESS_STRINGS))
-
-// 	return h, err
-// }
-
-// func (d *DiscoveryFactory) Connect() (Host string)  (Discovery, error){
-// 	return d,nil
-// }
-
-func (d *DiscoveryFactory) Create(host string) (host.Host, error) {
-	if host == "" {
-		host = LISTEN_ADDRESS_STRINGS
+// code form https://github.com/IceFireDB/IceFireDB/tree/main/IceFireDB-PubSub/pkg/p2p
+// Initialize network in ipfs
+func InitDiscovery(ctx context.Context, address string) (host.Host, *dht.IpfsDHT) {
+	if address == "" {
+		address = LISTEN_ADDRESS_STRINGS
 	}
-	var options = libp2p.ListenAddrStrings(host)
 
-	h, err := libp2p.New(options)
-	d.h = h
-	return h, err
+	// Setup Identity
+	prvkey, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, rand.Reader)
+	if err != nil {
+		log.Debug("p2pdb-discovery Setup Identity fail:", err.Error())
+	}
+	identity := libp2p.Identity(prvkey)
+
+	// Setup TLS
+	tls, err := libp2pTls.New(prvkey)
+	security := libp2p.Security(libp2pTls.ID, tls)
+	transport := libp2p.Transport(libp2pTcp.NewTCPTransport)
+	if err != nil {
+		log.Debug("p2pdb-discovery Setup TLS fail:", err.Error())
+	}
+
+	// SetUp listener address
+	muladdr, err := multiaddr.NewMultiaddr(address)
+	if err != nil {
+		log.Debug("p2pdb-discovery SetUp listener address fail:", err.Error())
+	}
+	listen := libp2p.ListenAddrs(muladdr)
+	// Set up the stream multiplexer and connection manager options
+	muxer := libp2p.Muxer("/yamux/1.0.0", yamux.DefaultTransport)
+	connmgr, err := libp2pConnmgr.NewConnManager(100, 400)
+	if err != nil {
+		log.Debug("p2pdb-discovery NewConnManager fail:", err.Error())
+	}
+	conn := libp2p.ConnectionManager(connmgr)
+
+	// Setup NAT
+	nat := libp2p.NATPortMap()
+	relay := libp2p.EnableAutoRelay()
+
+	// Declare a KadDHT
+	var kaddht *dht.IpfsDHT
+	// Setup a routing configuration with the KadDHT
+	routing := libp2p.Routing(func(h host.Host) (routing.PeerRouting, error) {
+		kaddht = setupKadDHT(ctx, h)
+		return kaddht, err
+	})
+
+	opts := libp2p.ChainOptions(identity, listen, security, transport, muxer, conn, nat, routing, relay)
+
+	// Construct a new libP2P host with the created options
+	libhost, err := libp2p.New(opts)
+	if err != nil {
+		log.Debug("p2pdb-discovery new libp2p fail:", err.Error())
+	}
+	return libhost, kaddht
 }
 
-// setupDiscovery creates an mDNS discovery service and attaches it to the libp2p Host.
-// This lets us automatically discover peers on the same LAN and connect to them.
-func (d *DiscoveryFactory) SetupDiscovery(h host.Host) error {
-	// setup mDNS discovery to find local peers
-	s := mdns.NewMdnsService(h, DiscoveryServiceTag, &discoveryNotifee{h: h})
-	log.Printf("local peer id is %s\n", h.ID())
-	return s.Start()
+// code form https://github.com/IceFireDB/IceFireDB/tree/main/IceFireDB-PubSub/pkg/p2p
+// A function that generates a Kademlia DHT object and returns it
+func setupKadDHT(ctx context.Context, nodehost host.Host) *dht.IpfsDHT {
+	// Create DHT server mode option
+	dhtmode := dht.Mode(dht.ModeServer)
+	// Rertieve the list of boostrap peer addresses
+	bootstrappeers := dht.GetDefaultBootstrapPeerAddrInfos()
+	// Create the DHT bootstrap peers option
+	dhtpeers := dht.BootstrapPeers(bootstrappeers...)
+
+	// Trace log
+	log.Debug("Generated DHT Configuration.")
+
+	// Start a Kademlia DHT on the host in server mode
+	kaddht, err := dht.New(ctx, nodehost, dhtmode, dhtpeers)
+	// Handle any potential error
+	if err != nil {
+		log.Debug("error", err.Error(), "Failed to Create the Kademlia DHT!")
+	}
+
+	// Return the KadDHT
+	return kaddht
+}
+
+// code form https://github.com/IceFireDB/IceFireDB/tree/main/IceFireDB-PubSub/pkg/p2p
+// A function that bootstraps a given Kademlia DHT to satisfy the IPFS router
+// interface and connects to all the bootstrap peers provided by libp2p
+func BootstrapDHT(ctx context.Context, nodehost host.Host, kaddht *dht.IpfsDHT) {
+	// Bootstrap the DHT to satisfy the IPFS Router interface
+	if err := kaddht.Bootstrap(ctx); err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": err.Error(),
+		}).Fatalln("Failed to Bootstrap the Kademlia!")
+	}
+
+	// Trace log
+	logrus.Traceln("Set the Kademlia DHT into Bootstrap Mode.")
+
+	// Declare a WaitGroup
+	var wg sync.WaitGroup
+	// Declare counters for the number of bootstrap peers
+	var connectedbootpeers int32
+	var totalbootpeers int32
+
+	// Iterate over the default bootstrap peers provided by libp2p
+	for _, peeraddr := range dht.DefaultBootstrapPeers {
+		// Retrieve the peer address information
+		peerinfo, _ := peer.AddrInfoFromP2pAddr(peeraddr)
+
+		// Incremenent waitgroup counter
+		wg.Add(1)
+		totalbootpeers++
+		// Start a goroutine to connect to each bootstrap peer
+		go func() {
+			// Defer the waitgroup decrement
+			defer wg.Done()
+			// Attempt to connect to the bootstrap peer
+			if err := nodehost.Connect(ctx, *peerinfo); err == nil {
+				// Increment the connected bootstrap peer count
+				atomic.AddInt32(&connectedbootpeers, 1)
+			}
+		}()
+	}
+
+	// Wait for the waitgroup to complete
+	wg.Wait()
+
+	// Log the number of bootstrap peers connected
+	log.Debug("Connected to %d out of %d Bootstrap Peers.", connectedbootpeers, totalbootpeers)
+}
+
+func NewRoutingDiscovery(router routing.ContentRouting) *discovery.RoutingDiscovery {
+	return &discovery.RoutingDiscovery{router}
 }
 
 // HandlePeerFound connects to peers discovered via mDNS. Once they're connected,
@@ -96,12 +167,12 @@ func (d *DiscoveryFactory) SetupDiscovery(h host.Host) error {
 func (n *discoveryNotifee) HandlePeerFound(pi peer.AddrInfo) {
 	var localPeerId = n.h.ID()
 	if pi.ID.Pretty() != localPeerId.String() {
-		log.Printf("discovered new remote peer id %s\n", pi.ID.Pretty())
+		log.Info("discovered new remote peer id %s\n", pi.ID.Pretty())
 	}
 
 	err := n.h.Connect(context.Background(), pi)
 
 	if pi.ID.Pretty() != localPeerId.String() && err != nil {
-		log.Printf("error connecting to remote peer id %s: %s\n", pi.ID.Pretty(), err)
+		log.Info("error connecting to remote peer id %s: %s\n", pi.ID.Pretty(), err)
 	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require github.com/ipfs/go-ipfs v0.13.0
 
 require (
+	github.com/Rock-liyi/p2pdb v0.0.10
 	github.com/Rock-liyi/p2pdb-log v0.0.0-20220718124543-0bcc189e2a66
 	github.com/btcsuite/btcd v0.22.1
 	github.com/favframework/debug v0.0.0-20150708094948-5c7e73aafb21
@@ -13,9 +14,12 @@ require (
 	github.com/ipfs/go-log v1.0.5
 	github.com/ipfs/interface-go-ipfs-core v0.7.0
 	github.com/libp2p/go-libp2p v0.20.3
+	github.com/libp2p/go-libp2p-connmgr v0.3.1
 	github.com/libp2p/go-libp2p-core v0.16.1
-	github.com/libp2p/go-libp2p-swarm v0.11.0 // indirect
+	github.com/libp2p/go-libp2p-discovery v0.6.0
+	github.com/libp2p/go-libp2p-kad-dht v0.16.0
 	github.com/multiformats/go-multiaddr v0.5.0
-	github.com/multiformats/go-multibase v0.0.3
+	github.com/multiformats/go-multibase v0.1.0
+	github.com/sirupsen/logrus v1.9.0
 	github.com/stretchr/testify v1.8.0
 )
